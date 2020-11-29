e47770e77aac975d70fd35130de85178a1440055
Tammy Laforest <TammyLaforest@gmail.com>
2020-11-29 13:12:12 -0500

    added temporary data

---
+++ tempData.js
@@ -0,0 +1,58 @@
+export default tempData = [
+  {
+    name: "Plan a Trip",
+    color: "#24A6D9",
+    todos: [
+      {
+        title: "Book Flight",
+        completed: false,
+      },
+      {
+        title: "Passport Check",
+        completed: true,
+      },
+      {
+        title: "Pack luggage",
+        completed: false,
+      },
+    ],
+  },
+
+  {
+    name: "Errands",
+    color: "#8022D9",
+    todos: [
+      {
+        title: "Buy Milk",
+        completed: false,
+      },
+      {
+        title: "Make a video",
+        completed: true,
+      },
+      {
+        title: "Walk dog",
+        completed: false,
+      },
+    ],
+  },
+
+  {
+    name: "Birthday Party",
+    color: "#595BD9",
+    todos: [
+      {
+        title: "Buy Balloons",
+        completed: false,
+      },
+      {
+        title: "Send Invitations",
+        completed: true,
+      },
+      {
+        title: "Wrap gifts",
+        completed: false,
+      },
+    ],
+  },
+];
